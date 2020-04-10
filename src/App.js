025d6929dfbcaf22962f2bdef3f5d7e3e5cf2c1d
604karev <604karev@gmail.com>
2020-04-10 08:46:57 +0300

    project settings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import DropDownWithSearch from './components/DropDownWithSearch'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <section className="dropdown-menu">
+                <div className="container">
+                    <div className="row">
+                        <div className="dropdown-menu-wrapper">
+                            <DropDownWithSearch/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/DropDownWithSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DropDownWithSearch = () => (
+    <select className='dropdown'>
+        <option value="test-buyer1">test-buyer1</option>
+        <option value="test-buyer2">test-buyer2</option>
+        <option value="test-buyer3">test-buyer3</option>
+
+    </select>
+);
+export default DropDownWithSearch
